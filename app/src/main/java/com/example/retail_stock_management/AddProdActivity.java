e88725fcb53de38b529c36e6c0eb1d30c1c800e8
Son Huynh <sonchuynh.mail@gmail.com>
2020-11-07 14:44:54 +0700

    add category selection spinner

--- app/src/main/java/com/example/retail_stock_management/AddProdActivity.java
+++ app/src/main/java/com/example/retail_stock_management/AddProdActivity.java
@@ -2,22 +2,29 @@ package com.example.retail_stock_management;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.IgnoreExtraProperties;
@@ -25,14 +32,15 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 
 public class AddProdActivity extends AppCompatActivity {
 
     public static final int REQUEST_UPLOAD = 1;
     private Uri imageUri;
-    //private ImageView ImageDisplay;
+    private String prodCategory;
+    private ArrayList<String> prodCategoryList = new ArrayList<>();
     private StorageReference ProductImageRef;
     private DatabaseReference DataRef;
 
@@ -43,6 +51,81 @@ public class AddProdActivity extends AppCompatActivity {
 
         ProductImageRef = FirebaseStorage.getInstance().getReference();
         DataRef = FirebaseDatabase.getInstance().getReference();
+
+        prodCategoryList.add("Pick category");
+        prodCategoryList.add("New category");
+
+        DatabaseReference CategoryDataRef = DataRef.child("category");
+        CategoryDataRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                String value = dataSnapshot.getKey();
+                prodCategoryList.add(value);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        final Spinner spinner = findViewById(R.id.prod_category);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, prodCategoryList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+                String selectedCategory = spinner.getSelectedItem().toString();
+
+
+                if (selectedCategory == "New category") {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddProdActivity.this);
+
+                    View viewInflated = LayoutInflater.from(AddProdActivity.this).inflate(R.layout.popup_add_prod_category, null, false);
+                    final EditText new_category_input = viewInflated.findViewById(R.id.popupNewCategory);
+
+                    builder.setTitle("New category name:");
+                    builder.setView(viewInflated);
+
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            prodCategory  = new_category_input.getText().toString();
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    builder.show();
+                }
+                else if (selectedCategory != "Pick category"){
+                    prodCategory = selectedCategory;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
     }
 
     public void pick_img(View view) {
@@ -57,18 +140,18 @@ public class AddProdActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_UPLOAD && resultCode == RESULT_OK && data != null) {
             imageUri = data.getData();
-            ImageView ImageDisplay = (ImageView) findViewById(R.id.upload_display);
+            ImageView ImageDisplay = findViewById(R.id.upload_display);
             ImageDisplay.setImageURI(imageUri);
         }
     }
 
     public void upload_img(View view) {
-        EditText ProductName = (EditText) findViewById(R.id.prod_name);
-        EditText ProductPrice = (EditText) findViewById(R.id.prod_price);
-        EditText ProductBrand = (EditText) findViewById(R.id.prod_brand);
+        EditText ProductName = findViewById(R.id.prod_name);
+        EditText ProductPrice = findViewById(R.id.prod_price);
+        //EditText ProductCategory = findViewById(R.id.prod_cat);
         final String prodName = ProductName.getText().toString();
         final String prodPrice = ProductPrice.getText().toString();
-        final String prodBrand = ProductBrand.getText().toString();
+        //final String prodCategory = ProductCategory.getText().toString();
 
         if (imageUri == null) {
             Toast.makeText(this, "Missing product image", Toast.LENGTH_SHORT).show();
@@ -79,8 +162,8 @@ public class AddProdActivity extends AppCompatActivity {
         else if (TextUtils.isEmpty(prodPrice)) {
             Toast.makeText(this, "Missing product price", Toast.LENGTH_SHORT).show();
         }
-        else if (TextUtils.isEmpty(prodBrand)) {
-            Toast.makeText(this, "Missing product brand", Toast.LENGTH_SHORT).show();
+        else if (TextUtils.isEmpty(prodCategory)) {
+            Toast.makeText(this, "Missing product category", Toast.LENGTH_SHORT).show();
         }
         else {
             final StorageReference filePath = ProductImageRef.child("prodImages/" + imageUri.getLastPathSegment());
@@ -103,7 +186,7 @@ public class AddProdActivity extends AppCompatActivity {
                         Uri downloadUri = task.getResult();
                         final String image_url = downloadUri.toString();
 
-                        saveToDatabase(prodName, prodPrice, prodBrand, image_url);
+                        saveToDatabase(prodName, prodPrice, prodCategory, image_url);
                     } else {
                         // Handle failures
                         Toast.makeText(AddProdActivity.this, "Error!", Toast.LENGTH_SHORT).show();
@@ -113,11 +196,11 @@ public class AddProdActivity extends AppCompatActivity {
         }
     }
 
-    public void saveToDatabase(String prodName, String prodPrice, String prodBrand, String image_url) {
+    public void saveToDatabase(String prodName, String prodPrice, String prodCategory, String image_url) {
         DatabaseReference ProductDataRef = DataRef.child("product");
         String prodID = ProductDataRef.push().getKey();
 
-        Product newProduct = new Product(prodName, prodPrice, prodBrand, image_url);
+        Product newProduct = new Product(prodName, prodPrice, prodCategory, image_url);
 
         ProductDataRef.child(prodID).setValue(newProduct);
     }
@@ -127,17 +210,17 @@ public class AddProdActivity extends AppCompatActivity {
 class Product {
     public String productName;
     public String productPrice;
-    public String productBrand;
+    public String prodCategory;
     public String productImage;
 
     public Product() {
 
     }
 
-    public Product (String productName, String productPrice, String productBrand, String productImage){
+    public Product (String productName, String productPrice, String prodCategory, String productImage){
         this.productName = productName;
         this.productPrice = productPrice;
-        this.productBrand = productBrand;
+        this.prodCategory = prodCategory;
         this.productImage = productImage;
     }
 }
